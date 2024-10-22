b500a3451ad10c7b4a5d8ad344942c8db945a1ce
donny <donnyfe@qq.com>
2024-10-22 23:45:04 +0800

    fix: 修改类型定义

--- src/types/global.ts
+++ src/types/global.ts
@@ -1,5 +1,70 @@
+// 菜单项
 export interface MenuItem {
 	type: string
 	name: string
 	active?: boolean
 }
+
+// 视频源
+export interface VideoSource {
+	id: string
+	url: string
+	name: string
+	format: string
+	duration: number
+	width: number
+	height: number
+}
+
+// 音频源
+export interface AudioSource {
+	id: string
+	url: string
+	name: string
+	format: string
+	duration: number
+}
+
+// 图片源
+export interface ImageSource {
+	id: string
+	url: string
+	name: string
+	format: string
+	width: number
+	height: number
+}
+
+// 文本源
+export interface TextSource {
+	content: string
+	fill: string
+	stroke?: string
+	fontSize: number
+	fontFamily: string
+	textBackgroundColor?: string
+	name: string
+}
+
+// 尺寸
+export interface Size {
+	width: number
+	height: number
+}
+
+// 移动元素
+export interface MoveableItem {
+	id: string
+	lineIndex: number
+	itemIndex: number
+	y: number
+	x: number
+	w: number
+	h: number
+	scale: number
+	left: number
+	top: number
+	rotate?: number
+}
+
+export type CanvasContext = CanvasRenderingContext2D | OffscreenCanvasRenderingContext2D
--- src/types/track.ts
+++ src/types/track.ts
@@ -1,25 +1,55 @@
-import type { VideoTrack } from '@/components/video/VideoTrack'
-import type { AudioTrack } from '@/components/audio/AudioTrack'
-import type { ImageTrack } from '@/components/image/ImageTrack'
-import type { TextTrack } from '@/components/text/TextTrack'
+import type { VideoTrack, AudioTrack, ImageTrack, TextTrack } from '@/classes'
 
-export type Track = AudioTrack | ImageTrack | TextTrack | VideoTrack
+// 帧集合
+export type Frames = Record<string, any>[]
 
-export interface TrackLineItem {
-	type: Track['type']
+// 资源
+export type Resource = AudioTrack | ImageTrack | TextTrack | VideoTrack
+
+export type MoveableTrack = VideoTrack | ImageTrack | TextTrack
+
+// 轨道项
+export interface TrackListItem {
+	type: Resource['type']
+	list: Resource[]
 	main?: boolean
-	list: Track[]
 }
 
 // 轨道类型
-export type TrackType = 'video' | 'audio' | 'text' | 'image' | 'effect' | 'transition' | 'filter'
+export type ResourceType = 'video' | 'audio' | 'text' | 'image' | 'effect' | 'transition' | 'filter'
 
 // 轨道项
 export interface BaseTrack {
 	id: string
-	type: TrackType
+	type: ResourceType
 	name: string
+	frameCount: number // 总帧数
 	start: number // 在轨道上的起始位置，单位为帧
 	end: number // 在轨道上的结束位置
-	frameCount: number // 总帧数
+}
+
+// 吸附线
+export interface AdsorptionLine {
+	position: number
+	frame: number
+}
+
+// 线坐标
+export interface LineCoord {
+	left: number
+	right: number
+	start: number
+	end: number
+}
+
+export interface InsertInfo extends LineCoord {
+	isNewLine: boolean
+	insertIndex: number
+	itemIndex: number
+}
+
+export interface InsertLineInfo {
+	isNewLine: boolean
+	insertIndex: number
+	elem?: HTMLElement
 }
